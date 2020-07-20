a34742162cffef7380204dc1c8b6971be59b2f02
Rahul Pawar <techrahul95@gmail.com>
2020-07-20 16:10:19 +0530

    No database Version

--- app/src/main/java/com/example/mdmscheme/Common/Profile.java
+++ app/src/main/java/com/example/mdmscheme/Common/Profile.java
@@ -1,16 +1,78 @@
 package com.example.mdmscheme.Common;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.example.mdmscheme.R;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class Profile extends AppCompatActivity {
+    private TextView nameTxt , schoolTxt, schoolIdTxt;
+    private TextView emailTxt;
+    private final String TAG = this.getClass().getName().toUpperCase();
+    private FirebaseDatabase database;
+    private DatabaseReference mDatabase;
+    private Map<String, String> userMap;
+    private String email;
+    private String USERID;
+    private static final String USERS = "users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Intent intent = getIntent();
+        email = intent.getStringExtra("email");
+
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("users");
+        DatabaseReference userRef = mDatabase.child(USERS);
+        Log.v("USERID", userRef.getKey());
+
+        nameTxt = (TextView)findViewById(R.id.name_profile);
+        schoolTxt = (TextView) findViewById(R.id.school_name_profile);
+        schoolIdTxt = (TextView)findViewById(R.id.school_id_profile);
+        emailTxt = (TextView)findViewById(R.id.emailPro);
+
+        ValueEventListener eventListener = userRef.addValueEventListener(new ValueEventListener() {
+            String fname,mail,schlid,schName;
+            @Override
+            public void onDataChange( DataSnapshot dataSnapshot) {
+                for (DataSnapshot keyid : dataSnapshot.getChildren()) {
+                    if (keyid.child("email").getValue().equals(email)) {
+                        fname = keyid.child("name").getValue(String.class);
+                        mail = keyid.child("email").getValue(String.class);
+                        schlid = keyid.child("schoolid").getValue(String.class);
+                        schName = keyid.child("schoolname").getValue(String.class);
+                        break;
+                    }
+                }
+
+                nameTxt.setText(fname);
+                emailTxt.setText(mail);
+                schoolIdTxt.setText(schlid);
+                schoolTxt.setText(schName);
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "Failed to read value.", error.toException());
+
+            }
+        });
+
     }
 }
